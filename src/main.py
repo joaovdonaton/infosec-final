51e6fdeae770183e2f20139755901724a71006cc
Joao Donaton <joao.vitor.donaton@gmail.com>
2025-04-12 16:19:16 -0400

    insecure user registering

--- src/main.py
+++ src/main.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
+import sqlite3
 
 app = Flask(__name__)
+
+DB_PATH = './database/database.db'
+
+# database stuff - from docs
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DB_PATH)
+    return db
 
 @app.route("/login")
 def login():
@@ -9,7 +19,20 @@ def login():
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        pass
+        connection = get_db()
+
+        query = (f'insert into users(username, name, password) values '
+                 f'("{request.form['username']}", "{request.form['name']}", "{request.form['password']}")')
+
+        print(query)
+
+        connection.cursor().execute(query)
+
+        connection.commit()
+        connection.close()
+
+
+        return render_template('index.html')
     else:
         return render_template("register.html")
 
