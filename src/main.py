d03df12e3c4d31cb656e5f7254f3c3b6a975775f
Joao Donaton <joao.vitor.donaton@gmail.com>
2025-04-13 14:54:05 -0400

    AES encryption and decryption for phone number

--- src/main.py
+++ src/main.py
@@ -2,10 +2,33 @@ from flask import Flask, render_template, request, g, session, url_for, redirect
 import sqlite3
 import hashlib
 
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.backends import default_backend
+
+from os import urandom
+import base64
+
 app = Flask(__name__)
 app.secret_key = '11111111111111'
 
 DB_PATH = './database/database.db'
+
+# aes key stuff
+def make_key(save_path):
+    with open(save_path, 'wb') as f:
+        f.write(urandom(32))
+
+def get_key(key_path):
+    key = ''
+    with open(key_path, 'rb') as f:
+        key = f.read()
+    return key
+
+# only run once to generate 32byte (256bit) aes key and save
+#make_key('./key')
+
+iv = b'aaaaaaaaaaaaaaaa'
 
 # database stuff - from docs
 def get_db():
@@ -45,17 +68,22 @@ def register():
 
         hashed_pwd = hashlib.sha256(request.form['password'].encode('utf-8')).hexdigest()
 
-        query = (f'insert into users(username, name, password) values '
-                 f'("{request.form['username']}", "{request.form['name']}", "{hashed_pwd}")')
+        # AES encryption
+        key = get_key('key')
+        padder = padding.PKCS7(128).padder()
+        x = str.encode(request.form['phone'])
+        x_pad = padder.update(x) + padder.finalize()
+
+        cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+        enc = cipher.encryptor()
+        encrypted_phone = enc.update(x_pad) + enc.finalize()
+
+        query = (f'insert into users(username, name, year, phone, password) values '
+                 f'("{request.form['username']}", "{request.form['name']}", "{request.form['year']}",'
+                 f' "{base64.b64encode(encrypted_phone).decode('utf-8')}",  "{hashed_pwd}")')
 
         cur.executescript(query)
         connection.commit()
-
-        #
-        # print(f'SELECT * FROM users where username="{request.form["username"]}"')
-        # cur.execute(f'SELECT * FROM users where username="{request.form["username"]}"')
-        # row = cur.fetchall()
-        # print(row)
 
         connection.close()
 
@@ -75,12 +103,30 @@ def index():
     if 'namelike' in request.args:
         namelike_param = request.args['namelike']
 
-    print(f'SELECT username, name FROM users where username like "{namelike_param}";')
-    cur.execute(f'SELECT username, name FROM users where username like "%{namelike_param}%";')
+    #print(f'SELECT username, name, phone FROM users where username like "{namelike_param}";')
+    cur.execute(f'SELECT username, name, phone FROM users where username like "%{namelike_param}%";')
     rows = cur.fetchall()
-    print(rows)
 
-    return render_template("index.html", loggedIn=True, rows=rows)
+    # decrypt the phones
+    key = get_key('key')
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+
+    decrypted_rows = []
+    for r in rows:
+        decrypted_rows.append([])
+        for i in range(len(r)):
+            if i == 2: # phone, need to decrypt here
+                decrypt = cipher.decryptor()
+                unpadder = padding.PKCS7(128).unpadder()
+                x_pad = decrypt.update(base64.b64decode(r[i])) + decrypt.finalize()
+                decrypted_rows[-1].append((unpadder.update(x_pad) + unpadder.finalize()).decode('utf-8'))
+            else:
+                decrypted_rows[-1].append(r[i])
+
+    return render_template("index.html", loggedIn=True, rows=decrypted_rows)
+
+
 
 if __name__ == '__main__':
+
     app.run(host='localhost', port=5000, debug=True)
